2b717c27a18a4d712d29b0192093bd0babd35246
Menmar <felixmenendezmarques@gmail.com>
2026-04-18 23:12:16 +0200

    include sakai

---
+++ src/main/java/com/menmar/gestionTienda/config/SpaFallbackController.java
@@ -0,0 +1,13 @@
+package com.menmar.gestionTienda.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SpaFallbackController {
+
+    @RequestMapping(value = { "/", "/{path:[^\\.]*}", "/{path:[^\\.]*}/**" })
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
